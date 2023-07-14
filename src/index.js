82e4ef4e07b4ded962ad7dcfc27fb3729c15eb7d
Krishan <krishankantsen.bhumca22@gmail.com>
2023-07-14 07:26:50 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+const img1="https://picsum.photos/200/300";
+let curdate=new Date();
+curdate=curdate.getHours();
+const css={
+  color :"green"
+};
+let greet="Good Morning";
+if (curdate>=1 && curdate<12) {
+  greet="Good Morning";
+  css.color='green';
+}else if (curdate>=12 && curdate<19) {
+  greet="Good AfterNoon";
+  css.color='red';
+}else{
+  greet="Good Night";
+  css.color='blue';
+}
+const body={
+  background :"#bf56f0"
+}
+
+const heading={
+  background :"cyan",
+  padding :"10px",
+  marginLeft:"400px",
+  marginTop : "240px",
+  marginRight :"455px",
+  
+  borderRadius : "10px",
+  // color : "pink"
+
+};
+ReactDOM.render(
+  <>
+    <h1 style={heading}>Hello Sir,<span style={css}>{greet}</span></h1>
+   
+  </>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
